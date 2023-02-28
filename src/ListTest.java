193e323c6cb52863dd784fd5c68f8f000f2918d3
alexhatch <alexhatchh@gmail.com>
2023-02-27 17:49:20 -0800

    Implemented tests

--- src/ListTest.java
+++ src/ListTest.java
@@ -55,7 +55,63 @@ public class ListTest {
     }
 
 
-    // the length() method:
+    // the get() method:
+
+    // Specification:
+
+    // Checks whether or not a value is in the list
+
+    // Tests:
+
+    @Test
+    public void testGet() {
+        Integer[] ar = {2, 3, 4};
+        List<Integer> l = makeList(ar);
+        assertEquals(2, l.get(0));
+        assertEquals(3, l.get(1));
+        assertEquals(4, l.get(2));
+    }
+
+    // the indexOf() method:
+
+    // Specification:
+
+    // Returns the index of the item in the list searched for
+
+    // Tests:
+
+    @Test
+    public void testIndexOf() {
+        Integer[] ar = {2, 3, 4};
+        List<Integer> l = makeList(ar);
+
+        assertEquals(0, l.indexOf(2));
+        assertEquals(1, l.indexOf(3));
+        assertEquals(2, l.indexOf(4));
+        assertEquals(-1, l.indexOf(0));
+    }
+
+    // the remove() method:
+
+    // Specification:
+
+    // Removes an item in the list at the specified index.
+    // Returns the item that was removed from the list.
+
+    // Tests:
+
+    @Test
+    public void testRemove() {
+        Integer[] ar = {2, 3, 4};
+        List<Integer> l = makeList(ar);
+
+        assertEquals(2, l.remove(0));
+        assertEquals(3, l.remove(0));
+        assertEquals(4, l.remove(0));
+    }
+
+
+    // the size() method:
 
     // Specification:
 
@@ -64,10 +120,44 @@ public class ListTest {
     // Tests:
 
     @Test
-    public void testLength1() {
+    public void testSize() {
         Integer[] ar = {3, 4, 5};
         List<Integer> l = makeList(ar);
         assertEquals(3, l.size());
+    }
+
+    // the subList() method:
+
+    // Specification:
+
+    // Returns a view of the portion of this list between the first argument, inclusive
+    // to the second argument, exclusive
+
+    // Tests:
+
+    @Test
+    public void testSubList() {
+        String[] ar = {"a", "b", "c", "d", "e"};
+        List<String> list = makeList(ar);
+
+        List<String> subList = list.subList(1, 4);
+
+        assertEquals(3, subList.size());
+        assertEquals("b", subList.get(0));
+        assertEquals("c", subList.get(1));
+        assertEquals("d", subList.get(2));
+
+        subList.set(1, "x");
+
+        assertEquals("x", subList.get(1));
+        assertEquals("x", list.get(2));
+
+        subList.add("y");
+
+        assertEquals(4, subList.size());
+        assertEquals(6, list.size());
+        assertEquals("y", subList.get(3));
+        assertEquals("e", list.get(5));
     }
 
 }
